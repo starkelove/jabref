84f13b0bfc82ce28ff0017708ba22cfba6b8f382
d0ver <antonhedkrans@gmail.com>
2019-02-15 16:34:18 +0100

    Feat: Prints the hashmap keys and the number of branches visited #2

--- src/main/java/org/jabref/CoverageMeasurement.java
+++ src/main/java/org/jabref/CoverageMeasurement.java
@@ -1,9 +1,24 @@
 package org.jabref;
 
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class CoverageMeasurement {
   // <BranchID, If branch visited mark as True>
   public static final HashMap<String, Boolean[]> coverageMap = new HashMap<String, Boolean[]>();
 
+  public static void PrintMap() {
+    Iterator iterator = coverageMap.keySet().iterator();
+
+    while(iterator.hasNext()) {
+      int branchNum = 0;
+      String key = iterator.next().toString();
+      for(Boolean branchVisit : coverageMap.get(key)) {
+        if(branchVisit != null && branchVisit) {
+          branchNum++;
+        }
+      }
+      System.out.println("BranchID: " + key + " " + "Visited: " + branchNum);
+    }
+  }
 }
