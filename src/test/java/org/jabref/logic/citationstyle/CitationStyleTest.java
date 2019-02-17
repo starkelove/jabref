b58fdb34ba49411ce1a912791dfc7fdc5e36402f
starkelove <lovest@kth.se>
2019-02-17 14:19:18 +0100

    Feat: Added a test for discoverCitationStyles #3

--- src/test/java/org/jabref/logic/citationstyle/CitationStyleTest.java
+++ src/test/java/org/jabref/logic/citationstyle/CitationStyleTest.java
@@ -1,13 +1,30 @@
 package org.jabref.logic.citationstyle;
 
-import org.jabref.logic.util.TestEntry;
+//import org.jabref.logic.util.TestEntry;
 
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.Mock;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class CitationStyleTest {
+
+    private CitationStyle citationStyle;
+
 
     @Test
     public void getDefault() throws Exception {
@@ -29,4 +46,20 @@ public class CitationStyleTest {
         assertEquals(expected, citation);
     }
     */
+
+    /**
+     * Tests to see if an empty list is returned. Assert that we have actually received a list
+     * but also that this list is empty
+     * @author Love Stark
+     * @throws Exception
+     */
+    @Test
+    public void testDiscoverCitationStyles() throws Exception {
+        List<CitationStyle> list =  CitationStyle.discoverCitationStyles();
+        assertNotNull(list);
+        assertTrue(list.isEmpty());
+
+    }
+
+
 }
