2bd4fdc0ac966dbc1d48960fd1cc6d50412a720f
starkelove <lovest@kth.se>
2019-02-18 11:26:30 +0100

    Feat: Added a before each statement to avoid redundant code #3

--- src/test/java/org/jabref/logic/bibtex/comparator/BibStringDiffTest.java
+++ src/test/java/org/jabref/logic/bibtex/comparator/BibStringDiffTest.java
@@ -2,17 +2,25 @@ package org.jabref.logic.bibtex.comparator;
 
 import org.jabref.model.database.BibDatabase;
 import org.jabref.model.entry.BibtexString;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BibStringDiffTest {
 
     private BibDatabase database1;
     private BibDatabase database2;
+    private BibtexString stringA;
+
+    @BeforeEach
+    public void setUp() {
+        database1 = new BibDatabase();
+        database2 = new BibDatabase();
+        stringA = new BibtexString("TEST", "SOME STRING");
+    }
 
     /**
      * Tests that two databases with identical content will be identified
@@ -21,15 +29,12 @@ public class BibStringDiffTest {
      */
     @Test
     public void testIdenticalDatabases(){
-        database1 = new BibDatabase();
-        database2 = new BibDatabase();
-        BibtexString a = new BibtexString("TEST", "SOME STRING");
-        database1.addString(a);
-        database2.addString(a);
-
-        List<BibStringDiff> list = BibStringDiff.compare(database1, database1);
+        database1.addString(stringA);
+        database2.addString(stringA);
+        List<BibStringDiff> list = BibStringDiff.compare(database1, database2);
         assertNotNull(list);
         assertTrue(list.isEmpty());
     }
+
 
 }
