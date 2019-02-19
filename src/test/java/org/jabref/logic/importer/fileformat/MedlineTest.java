6d442babff36ff75758a462234f7f82dc6750d37
RoboticNoob <victor.carle@gmail.com>
2019-02-19 11:05:06 +0100

    test: Fixes #9

--- src/test/java/org/jabref/logic/importer/fileformat/MedlineTest.java
+++ src/test/java/org/jabref/logic/importer/fileformat/MedlineTest.java
@@ -1,11 +1,13 @@
 package org.jabref.logic.importer.fileformat;
 
 import org.jabref.logic.importer.fileformat.medline.Investigator;
+import org.jabref.logic.importer.fileformat.medline.KeywordList;
 import org.jabref.logic.util.StandardFileType;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MedlineTest {
@@ -18,4 +20,13 @@ public class MedlineTest {
         temp.setValidYN("hej");
         assertEquals("hej", temp.getValidYN());
     }
+
+    @Test
+    public void testGetAndSetOwner(){
+        KeywordList temp = new KeywordList();
+        temp.setOwner("apa");
+        assertEquals("apa", temp.getOwner());
+        assertNotEquals("banan", temp.getOwner());
+    }
+
 }
